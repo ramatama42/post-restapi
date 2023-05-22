5ab155095950be8b3f83e4425fddc36833689ab4
ramatama42 <ramadhantama42@gmail.com>
2023-05-22 11:35:39 +0700

    Update

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World! 2')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
